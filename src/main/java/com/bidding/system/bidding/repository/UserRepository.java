b4339f1ba5bc50789ded56abc0b59eaef06a6176
Felipe1632 <felipesouzaribeiro24@gmail.com>
2026-05-12 17:29:15 -0300

    project

---
+++ src/main/java/com/bidding/system/bidding/repository/UserRepository.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bidding.system.bidding.repository;
+
+import com.bidding.system.bidding.model.UserDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Aluno
+ */
+@Repository
+public class UserRepository {
+    
+    public void register(UserDTO user){
+      try{
+         Connection conn = Conexao.conectar();
+         PreparedStatement stmt = null;
+         stmt = conn.prepareStatement("insert into usuarios values(id, nome, email, senha, role");
+         
+         stmt.setString(1, user.getNome());
+         stmt.setString(2, user.getEmail());
+         stmt.setString(3, user.getSenha());
+         stmt.setString(4, user.getRole());
+         
+         int linhasAfetadas = stmt.executeUpdate();
+         if(linhasAfetadas == 0){
+             throw new SQLException("Falha na atualização - Nenhuma linha foi ");
+         }
+      }catch(SQLException e){
+          e.printStackTrace();
+      }    
+   }
+    
+    public UserDTO logar(String email, String senha){
+        UserDTO user = new UserDTO();
+        
+        try{
+            Connection conn = Conexao.conectar();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            
+            stmt = conn.prepareStatement("select * from usuarios where email = ? && senha = ?");
+            
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            
+            rs = stmt.executeQuery();
+            
+            if(rs.next()){
+                user.setId(rs.getLong("id"));
+                user.setNome(rs.getString("email"));
+                user.setEmail(rs.getString("senha"));
+                user.setRole(rs.getString("role"));
+                
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return user;
+    }
+}
